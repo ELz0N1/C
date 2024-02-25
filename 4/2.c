053c5d30e925598566a01a5df1011a018217615d
Danil Zhukov <91378756+ELz0N1@users.noreply.github.com>
2024-02-25 13:53:18 +0700

    Add files via upload

---
+++ 4/2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <assert.h>
+
+int number_of_divisors(int number) {
+    int count = 0;
+    assert(number >= 0);
+    for (int i = 1; i <= number; i++) {
+       if (number % i == 0) {
+        count++;
+       }
+    }
+    return count;
+}
+
+int main() {
+    unsigned long long N;
+    scanf("%d", &N);
+    
+    int result = number_of_divisors(N);
+    printf("%d", result);
+    
+    return 0;
+}
