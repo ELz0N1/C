87e076a690a4f661c929b60bfdcfbb9cc24b0065
Danil Zhukov <91378756+ELz0N1@users.noreply.github.com>
2024-02-25 13:50:42 +0700

    Add files via upload

---
+++ 3/3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    unsigned long N;
+    scanf("%d", &N);
+
+    for (int i = 0; i < ((N*(N+1))/2)-1; i++) {
+        if (i % N == 0) printf("\n");
+        printf("%d ", i);
+    }
+
+    return 0;
+}
