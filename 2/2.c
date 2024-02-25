2e8cad2b360dbd4248c54200513b69bf55fccb63
Danil Zhukov <91378756+ELz0N1@users.noreply.github.com>
2024-02-25 13:49:48 +0700

    Add files via upload

---
+++ 2/2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    unsigned short number;
+    scanf("%d", &number);
+
+    if ((number % 4==0 && number%100!=0) || number%400==0) {
+        printf("Весокосный год");
+    } else {
+        printf("Год не весокосный");
+    }
+
+    return 0;
+}
