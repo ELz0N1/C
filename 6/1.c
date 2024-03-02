6a9bd648ebe49726e31b71533d59cd879b2a0162
Danil Zhukov <91378756+ELz0N1@users.noreply.github.com>
2024-03-03 01:18:29 +0700

    final version 1.c

--- 6/1.c
+++ 6/1.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 void scanArr(int* arr, size_t length) {
-    int numbers;
     for (int i = 0; i < length; i++) {
-        *(arr + i)= scanf("%d", &numbers);
+        scanf("%d", &(*(arr + i)));
     }
 }
 
@@ -16,7 +15,7 @@ void printArr(int* arr, size_t length) {
 
 void revertArr(int* arr, size_t length) {
     int* l = &arr[0];
-    int* r = &arr[length];
+    int* r = &arr[length - 1];
     while (l != r) {
         int tmp = *l;
         *l = *r;
@@ -39,57 +38,101 @@ int maxInArr(int* arr, size_t length) {
 int findInArr(int* arr, size_t length, int target) {
     for (int i = 0; i < length; i++) {
         if (*(arr + i) == target) {
-            return *(arr+i);
+            return i;
             break;
         }
     }
     return NULL;
 }
 
-//not finished...
 void extractDigits(int* arr, size_t length, int X) {
     int tmp = X, len_X = -1;
     
-    while (tmp > 0) {
+    while (tmp) {
         len_X += 1;
         tmp /= 10;
     }
-
+    
+    if (len_X == -1) len_X = 0;
+    
     for (int i = len_X; i>= 0; i--) {
-
+        arr[i] = X % 10;
+        X /= 10;
     }
-
 }
 
 int compareArrays(int* arr1, int* arr2, size_t len_arr1, size_t len_arr2) {
-    for (int i = 0; i < len_arr1; i++) {
-        for (int j = 0; j < len_arr2; j++) {
-            if (*(arr1 + i) == *(arr2 + j)) {
-                continue;
-            } else if (*(arr1 + i) > *(arr2 + j)) {
-                return -1;
-            } else {
-                return 1;
-            }
-
+    size_t min_len = (len_arr1 < len_arr2) ? len_arr1 : len_arr1;
+    for (int i = 0; i < min_len; i++) {
+        if (*(arr1 + i) > *(arr2 + i)) {
+            return -1;
+        } else if (*(arr1 + i) < *(arr2 + i)) {
+            return 1;
+        } else {
+            continue;
         }
     }
     return 0;
 }
 
-int main(){
-    int Array1[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int len_Array1 = 10;
-    int Array2[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int len_Array2 = 10;
-    int target = 5, X = 12345;
-
+int main() {
+    int len_Array1;
+    printf("Enter length of array: ");
+    scanf("%d", &len_Array1);
+    int Array1[len_Array1];
+    printf("Enter elements of array: ");
     scanArr(Array1, len_Array1);
+
+    printf("Entered array: ");
     printArr(Array1, len_Array1);
+
     revertArr(Array1, len_Array1);
-    maxInArr(Array1, len_Array1);
-    findInArr(Array1, len_Array1, target);
-    extractDigits(Array1, len_Array1, X);
-    compareArrays(Array1, Array2, len_Array1, len_Array2);
+    printf("Reversed array: ");
+    printArr(Array1, len_Array1);
+    revertArr(Array1, len_Array1); //возвращение к исходному
+
+    int result;
+    result = maxInArr(Array1, len_Array1);
+    printf("Max: %d\n", result);
+
+    int target;
+    printf("Enter element to find: ");
+    scanf("%d", &target);
+    result = findInArr(Array1, len_Array1, target);
+    if (result == -1) {
+        printf("Element not found\n");
+    } else {
+        printf("Element %d index is %d\n", target, result);
+    }
+
+    if (len_Array1 < 0)
+        printf("Not able to perform extractDigits function for this array\n");
+    else {
+        int X;
+        int arr_copy[len_Array1];
+        for (size_t i = 0; i < len_Array1; i++) arr_copy[i] = Array1[i];
+        printf("Enter your digits: ");
+        scanf("%d", &X);
+        extractDigits(arr_copy, len_Array1, X);
+        printf("Array with %d extracted:", X);
+        printArr(arr_copy, len_Array1);
+    }
+
+    printf("Enter length of array to compare: ");
+    int len_Array2;
+    scanf("%d", &len_Array2);
+    printf("Enter elements of array to compare: ");
+    int Array2[len_Array2];
+    scanArr(Array2, len_Array2);
+
+    result = compareArrays(Array1, Array2, len_Array1, len_Array2);
+    if (result == -1) {
+        printf("First array is greater");
+    } else if (result == 1) {
+        printf("Second array is greater");
+    } else {
+        printf("Arrays are equal");
+    }
+        
     return 0;
 }
