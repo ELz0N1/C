5bc757f0b0804d91eec585704464876d2001adee
Danil Zhukov <91378756+ELz0N1@users.noreply.github.com>
2024-03-03 21:58:46 +0700

    Update 1.c

--- 2/1.c
+++ 2/1.c
@@ -4,30 +4,34 @@ int x, y;
 
 int main() {
     scanf("%d%d", &x, &y);
+    
+    printf("%d + %d = %d\n", x, y, x + y);
+    printf("%d - %d = %d\n", x, y, x - y);
+    printf("%d * %d = %d\n", x, y, x * y);
+    printf("%d == %d = %d\n", x, y, x == y);
+    printf("%d != %d = %d\n", x, y, x != y);
+    printf("%d < %d = %d\n", x, y, x < y);
+    printf("%d > %d = %d\n", x, y, x > y);
+    printf("%d <= %d = %d\n", x, y, x <= y);
+    printf("%d >= %d = %d\n", x, y, x >= y);
+    printf("%d && %d = %d\n", x, y, x && y);
+    printf("%d || %d = %d\n", x, y, x || y);
+    printf("%d & %d = %d\n", x, y, x & y);
+    printf("%d | %d = %d\n", x, y, x | y);
+    printf("%d ^ %d = %d\n", x, y, x ^ y);
+    printf("%d << %d = %d\n", x, y, x << y);
+    printf("%d >> %d = %d\n", x, y, x >> y);
+    printf("!%d = %d\n", x, !x);
+    printf("~%d = %d\n", x, ~x);
+    printf("-%d = %d\n", x, -x);
+    
     if (y == 0) {
-        printf("Second parameter can't be 0");
-    } else{
-        printf("%d + %d = %d\n", x, y, x + y);
-        printf("%d - %d = %d\n", x, y, x - y);
-        printf("%d * %d = %d\n", x, y, x * y);
+        printf("Division by zero is undefined\n");
+    } else {
         printf("%d / %d = %d\n", x, y, x / y);
         printf("%d %% %d = %d\n", x, y, x % y);
-        printf("%d == %d = %d\n", x, y, x == y);
-        printf("%d != %d = %d\n", x, y, x != y);
-        printf("%d < %d = %d\n", x, y, x < y);
-        printf("%d > %d = %d\n", x, y, x > y);
-        printf("%d <= %d = %d\n", x, y, x <= y);
-        printf("%d >= %d = %d\n", x, y, x >= y);
-        printf("%d && %d = %d\n", x, y, x && y);
-        printf("%d || %d = %d\n", x, y, x || y);
-        printf("%d & %d = %d\n", x, y, x & y);
-        printf("%d | %d = %d\n", x, y, x | y);
-        printf("%d ^ %d = %d\n", x, y, x ^ y);
-        printf("%d << %d = %d\n", x, y, x << y);
-        printf("%d >> %d = %d\n", x, y, x >> y);
-        printf("!%d = %d\n", x, !x);
-        printf("~%d = %d\n", x, ~x);
-        printf("-%d = %d\n", x, -x);
     }
+    
+    
     return 0;
 }
